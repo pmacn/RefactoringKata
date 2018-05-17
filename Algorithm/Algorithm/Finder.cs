ece6652a82b1e3758e35021fc0745cf048a13afe
Peter MacNaughton <peter@macnaughton.info>
2018-05-17 10:17:46 -0600

    Extract method GeneratePairs to create list of candidate Pair's

--- Algorithm/Algorithm/Finder.cs
+++ Algorithm/Algorithm/Finder.cs
@@ -13,14 +13,47 @@ namespace Algorithm
 
         public Pair Find(FinderOption option)
         {
-            var tr = new List<Pair>();
+            List<Pair> pairs = GeneratePairs();
 
-            for(var i = 0; i < _people.Count - 1; i++)
+            if (pairs.Count < 1)
             {
-                for(var j = i + 1; j < _people.Count; j++)
+                return new Pair();
+            }
+
+            Pair answer = pairs[0];
+            foreach (var result in pairs)
+            {
+                switch (option)
+                {
+                    case FinderOption.Closest:
+                        if (result.BirthDateDistance < answer.BirthDateDistance)
+                        {
+                            answer = result;
+                        }
+                        break;
+
+                    case FinderOption.Furthest:
+                        if (result.BirthDateDistance > answer.BirthDateDistance)
+                        {
+                            answer = result;
+                        }
+                        break;
+                }
+            }
+
+            return answer;
+        }
+
+        private List<Pair> GeneratePairs()
+        {
+            var pairs = new List<Pair>();
+
+            for (var i = 0; i < _people.Count - 1; i++)
+            {
+                for (var j = i + 1; j < _people.Count; j++)
                 {
                     var r = new Pair();
-                    if(_people[i].BirthDate < _people[j].BirthDate)
+                    if (_people[i].BirthDate < _people[j].BirthDate)
                     {
                         r.Person1 = _people[i];
                         r.Person2 = _people[j];
@@ -31,37 +64,11 @@ namespace Algorithm
                         r.Person2 = _people[i];
                     }
                     r.BirthDateDistance = r.Person2.BirthDate - r.Person1.BirthDate;
-                    tr.Add(r);
+                    pairs.Add(r);
                 }
             }
 
-            if(tr.Count < 1)
-            {
-                return new Pair();
-            }
-
-            Pair answer = tr[0];
-            foreach(var result in tr)
-            {
-                switch(option)
-                {
-                    case FinderOption.Closest:
-                        if(result.BirthDateDistance < answer.BirthDateDistance)
-                        {
-                            answer = result;
-                        }
-                        break;
-
-                    case FinderOption.Furthest:
-                        if(result.BirthDateDistance > answer.BirthDateDistance)
-                        {
-                            answer = result;
-                        }
-                        break;
-                }
-            }
-
-            return answer;
+            return pairs;
         }
     }
 }
