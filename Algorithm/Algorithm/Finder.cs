0c265faa4f253c0fa660cb3522b8d3828a227518
Peter MacNaughton <peter@macnaughton.info>
2018-05-17 09:41:28 -0600

    Rename field in finder

--- Algorithm/Algorithm/Finder.cs
+++ Algorithm/Algorithm/Finder.cs
@@ -4,31 +4,31 @@ namespace Algorithm
 {
     public class Finder
     {
-        private readonly List<Person> _p;
+        private readonly List<Person> _people;
 
-        public Finder(List<Person> p)
+        public Finder(List<Person> people)
         {
-            _p = p;
+            _people = people;
         }
 
         public F Find(FinderOption option)
         {
             var tr = new List<F>();
 
-            for(var i = 0; i < _p.Count - 1; i++)
+            for(var i = 0; i < _people.Count - 1; i++)
             {
-                for(var j = i + 1; j < _p.Count; j++)
+                for(var j = i + 1; j < _people.Count; j++)
                 {
                     var r = new F();
-                    if(_p[i].BirthDate < _p[j].BirthDate)
+                    if(_people[i].BirthDate < _people[j].BirthDate)
                     {
-                        r.P1 = _p[i];
-                        r.P2 = _p[j];
+                        r.P1 = _people[i];
+                        r.P2 = _people[j];
                     }
                     else
                     {
-                        r.P1 = _p[j];
-                        r.P2 = _p[i];
+                        r.P1 = _people[j];
+                        r.P2 = _people[i];
                     }
                     r.D = r.P2.BirthDate - r.P1.BirthDate;
                     tr.Add(r);
