a5b89b7de8ef1fdc83ce24baf86480f248fd9c57
Peter MacNaughton <peter@macnaughton.info>
2018-05-17 09:26:39 -0600

    Rename Thing to Person

--- Algorithm.Tests/FinderTests.cs
+++ Algorithm.Tests/FinderTests.cs
@@ -9,7 +9,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Empty_Results_When_Given_Empty_List()
         {
-            var list = new List<Thing>();
+            var list = new List<Person>();
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -21,7 +21,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Empty_Results_When_Given_One_Person()
         {
-            var list = new List<Thing>() { sue };
+            var list = new List<Person>() { sue };
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -33,7 +33,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Closest_Two_For_Two_People()
         {
-            var list = new List<Thing>() { sue, greg };
+            var list = new List<Person>() { sue, greg };
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -45,7 +45,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Furthest_Two_For_Two_People()
         {
-            var list = new List<Thing>() { greg, mike };
+            var list = new List<Person>() { greg, mike };
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
@@ -57,7 +57,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Furthest_Two_For_Four_People()
         {
-            var list = new List<Thing>() { greg, mike, sarah, sue };
+            var list = new List<Person>() { greg, mike, sarah, sue };
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
@@ -69,7 +69,7 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Closest_Two_For_Four_People()
         {
-            var list = new List<Thing>() { greg, mike, sarah, sue };
+            var list = new List<Person>() { greg, mike, sarah, sue };
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -78,9 +78,9 @@ namespace Algorithm.Test
             Assert.Same(greg, result.P2);
         }
 
-        Thing sue = new Thing() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
-        Thing greg = new Thing() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
-        Thing sarah = new Thing() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
-        Thing mike = new Thing() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
+        Person sue = new Person() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
+        Person greg = new Person() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
+        Person sarah = new Person() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
+        Person mike = new Person() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
     }
 }
