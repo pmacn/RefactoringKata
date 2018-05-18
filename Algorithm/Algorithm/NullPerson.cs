e9307a7a0e6b43ca00a2bddcb5dcf5ecf951da04
Peter MacNaughton <peter@macnaughton.info>
2018-05-18 14:13:37 -0600

    Move NullPerson to its own file

---
+++ Algorithm/Algorithm/NullPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class NullPerson : Person
+    {
+        public NullPerson() : base(String.Empty, DateTime.MinValue)
+        {
+        }
+
+        public override bool Equals(object obj) => false;
+
+        public override int GetHashCode() => 0;
+
+        public override string ToString() => "NullPerson";
+    }
+}
--- Algorithm/Algorithm/Person.cs
+++ Algorithm/Algorithm/Person.cs
@@ -34,17 +34,4 @@ namespace Algorithm
             return Name.GetHashCode() * 31 + BirthDate.GetHashCode();
         }
     }
-
-    public class NullPerson : Person
-    {
-        public NullPerson() : base(String.Empty, DateTime.MinValue)
-        {
-        }
-
-        public override bool Equals(object obj) => false;
-
-        public override int GetHashCode() => 0;
-
-        public override string ToString() => "NullPerson";
-    }
 }
