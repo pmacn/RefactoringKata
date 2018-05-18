aa07e833fe7bf75a975839a6631b09dfe09212fd
Peter MacNaughton <peter@macnaughton.info>
2018-05-18 12:44:27 -0600

    Grouped tests into classes by "Given" phrase
    Renamed some tests to better indicate purpose

--- Algorithm.Tests/FinderTests.cs
+++ Algorithm.Tests/FinderTests.cs
@@ -6,81 +6,95 @@ namespace Algorithm.Test
 {    
     public class FinderTests
     {
-        [Fact]
-        public void Returns_Empty_Results_When_Given_Empty_List()
+        public class Given_Empty_List
         {
-            var list = new List<Person>();
-            var finder = new Finder(list);
+            [Fact]
+            public void Returns_Empty_Results()
+            {
+                var list = new List<Person>();
+                var finder = new Finder(list);
 
-            var result = finder.Find(FinderOption.Closest);
+                var result = finder.Find(FinderOption.Closest);
 
-            Assert.Null(result.Person1);
-            Assert.Null(result.Person2);
+                Assert.Null(result.Person1);
+                Assert.Null(result.Person2);
+            }
         }
 
-        [Fact]
-        public void Returns_Empty_Results_When_Given_One_Person()
+        public class Given_One_Person
         {
-            var list = new List<Person>() { sue };
-            var finder = new Finder(list);
+            [Fact]
+            public void Returns_Empty_Results()
+            {
+                var list = new List<Person>() { sue };
+                var finder = new Finder(list);
 
-            var result = finder.Find(FinderOption.Closest);
+                var result = finder.Find(FinderOption.Closest);
 
-            Assert.Null(result.Person1);
-            Assert.Null(result.Person2);
+                Assert.Null(result.Person1);
+                Assert.Null(result.Person2);
+            }
         }
 
-        [Fact]
-        public void Returns_Closest_Two_For_Two_People()
+        public class Given_Two_People
         {
-            var list = new List<Person>() { sue, greg };
-            var finder = new Finder(list);
 
-            var result = finder.Find(FinderOption.Closest);
+            [Fact]
+            public void Closest_Option_Returns_Pair_With_Lowest_Age_Difference()
+            {
+                var list = new List<Person>() { sue, greg };
+                var finder = new Finder(list);
 
-            Assert.Same(sue, result.Person1);
-            Assert.Same(greg, result.Person2);
+                var result = finder.Find(FinderOption.Closest);
+
+                Assert.Same(sue, result.Person1);
+                Assert.Same(greg, result.Person2);
+            }
+            
+            [Fact]
+            public void Furthest_Option_Returns_Pair_With_Highest_Age_Difference()
+            {
+                var list = new List<Person>() { greg, mike };
+                var finder = new Finder(list);
+
+                var result = finder.Find(FinderOption.Furthest);
+
+                Assert.Same(greg, result.Person1);
+                Assert.Same(mike, result.Person2);
+            }
         }
 
-        [Fact]
-        public void Returns_Furthest_Two_For_Two_People()
+        public class Given_Four_People
         {
-            var list = new List<Person>() { greg, mike };
-            var finder = new Finder(list);
 
-            var result = finder.Find(FinderOption.Furthest);
+            [Fact]
+            public void Closest_Option_Returns_Pair_With_Lowest_Age_Difference()
+            {
+                var list = new List<Person>() { greg, mike, sarah, sue };
+                var finder = new Finder(list);
 
-            Assert.Same(greg, result.Person1);
-            Assert.Same(mike, result.Person2);
+                var result = finder.Find(FinderOption.Furthest);
+
+                Assert.Same(sue, result.Person1);
+                Assert.Same(sarah, result.Person2);
+            }
+
+            [Fact]
+            public void Furthest_Option_Returns_Pair_With_Highest_Age_Difference()
+            {
+                var list = new List<Person>() { greg, mike, sarah, sue };
+                var finder = new Finder(list);
+
+                var result = finder.Find(FinderOption.Closest);
+
+                Assert.Same(sue, result.Person1);
+                Assert.Same(greg, result.Person2);
+            }
         }
 
-        [Fact]
-        public void Returns_Furthest_Two_For_Four_People()
-        {
-            var list = new List<Person>() { greg, mike, sarah, sue };
-            var finder = new Finder(list);
-
-            var result = finder.Find(FinderOption.Furthest);
-
-            Assert.Same(sue, result.Person1);
-            Assert.Same(sarah, result.Person2);
-        }
-
-        [Fact]
-        public void Returns_Closest_Two_For_Four_People()
-        {
-            var list = new List<Person>() { greg, mike, sarah, sue };
-            var finder = new Finder(list);
-
-            var result = finder.Find(FinderOption.Closest);
-
-            Assert.Same(sue, result.Person1);
-            Assert.Same(greg, result.Person2);
-        }
-
-        Person sue = new Person() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
-        Person greg = new Person() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
-        Person sarah = new Person() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
-        Person mike = new Person() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
+        public static Person sue = new Person() { Name = "Sue", BirthDate = new DateTime(1950, 1, 1) };
+        public static Person greg = new Person() { Name = "Greg", BirthDate = new DateTime(1952, 6, 1) };
+        public static Person sarah = new Person() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
+        public static Person mike = new Person() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
     }
 }
